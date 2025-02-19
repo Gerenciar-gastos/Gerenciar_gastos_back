c86aa4dcd9770b365e2f4e9c55da517cc0bbf02d
Monique de Souza <moniquejp16@gmail.com>
2025-02-19 12:57:20 -0300

    fix: removed console.log

--- src/schemas/HomeMonthSchemas.ts
+++ src/schemas/HomeMonthSchemas.ts
@@ -8,17 +8,17 @@ export const RegisterMonthSchema = Joi.object({
         .pattern(/^[a-zA-Z\s]+$/)
         .required()
         .messages({
-            'string.base': 'O nome deve ser uma string.',
-            'string.empty': 'O nome não pode estar vazio.',
-            'string.min': 'O nome deve ter pelo menos 3 caracteres.',
-            'string.max': 'O nome deve ter no máximo 50 caracteres.',
-            'string.pattern.base': 'O nome deve conter apenas letras e espaços.',
-            'any.required': 'O campo nome é obrigatório.',
+            'string.base': 'O mês deve ser uma string.',
+            'string.empty': 'O mês não pode estar vazio.',
+            'string.min': 'O mês deve ter pelo menos 3 caracteres.',
+            'string.max': 'O mês deve ter no máximo 50 caracteres.',
+            'string.pattern.base': 'O mês deve conter apenas letras e espaços.',
+            'any.required': 'O campo é obrigatório.',
         }),
 
     totalFunds: Joi.number()
         .required()
         .messages({
-            'any.required': 'O campo CPF é obrigatório.',
+            'any.required': 'O campo é obrigatório.',
         }),
 });
